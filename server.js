44dacd118fd26ded279e3b330fae0a834054c03c
Ravenor222 <ndanvers222@gmail.com>
2020-04-21 23:03:56 -0700

    mailgun finally working, state is sending over to email soothly -> env's in place

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const PORT = 3001;
@@ -8,8 +9,14 @@ app.use(bodyParser.json({limit: '50mb'}))
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors())
 cors({credentials: true, origin: true})
-
 app.use(express.static('public'))
+
+
+
+const mailgun = require("mailgun-js");
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+const mg = mailgun({apiKey: process.env.MAILGUN_KEY, domain: DOMAIN});
+
 
 
 app.get('/', function (req, res) {
@@ -17,8 +24,30 @@ app.get('/', function (req, res) {
 })
 
 app.post('/email', (req, res) => {
-  console.log(req.body);
-})
+  const { name, email, message } = req.body.data
+  
+  
+  const data = {
+    from: `Mailgun Sandbox <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+    to: "ndanvers222@gmail.com",
+    subject: `name:${name} , email:${email}`,
+    text: `${message}`
+  };
+  
+  mg.messages().send(data, function (error, body) {
+    console.log(body);
+    console.log(error)
+  });
+  
+  // You can see a record of this email in your logs: https://app.mailgun.com/app/logs.
+  
+  // You can send up to 300 emails/day from this sandbox server.
+  // Next, you should add your own domain so you can send 10000 emails/month for free.
+
+
+
+
+  ;})
 
 
  
